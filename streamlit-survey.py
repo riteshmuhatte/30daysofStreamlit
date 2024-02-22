d78028d8a033b7c19d947eb56332b602902bb649
riteshmuhatte <riteshmuhatte@yahoo.com>
2024-02-22 18:50:46 +0530

    Update streamlit-survey.py

--- streamlit-survey.py
+++ streamlit-survey.py
@@ -1,28 +1,26 @@
-import streamlit_survey as ss
 import streamlit as st
 
-survey = ss.StreamlitSurvey()
 st.header("Experimenting Survey Using Streamlit")
-survey.text_input("Name:", id="Q1")
+st.text_input("Name:")
 st.write("Have you watched the Day to Day Productivity video ??")
-watched = survey.radio("watched_st", options=["NA", "Yes", "No"], horizontal=True,label_visibility="collapsed", id="Q2")
+watched = st.radio("watched_st", options=["NA", "Yes", "No"], horizontal=True,label_visibility="collapsed")
 
 if watched == "Yes":
  st.write("Congratulations on the progress!!!")
  st.write("What do you think about the topics that were discussed?")
- survey.select_slider(
+ st.select_slider(
     "Scale:", options=["Super Boring", "Boring", "Meh!!", "Good", "Awesome"],
-    label_visibility="collapsed", id="Q3"
+    label_visibility="collapsed"
  )
 elif watched == "No":
  st.write("Now is the time...Watch it!!")
  st.write("Are you going to ever watch it ??")
- guilty = survey.radio("guilty", options=["NA", "Yes", "No"], horizontal=True, label_visibility="collapsed", id="Q4")
+ guilty = st.radio("guilty", options=["NA", "Yes", "No"], horizontal=True, label_visibility="collapsed")
 
  if guilty == "Yes":
   st.write("You are on the right Path!!")
  elif guilty == "No":
   st.write("Bhagwan apka bhala kare !")
 
-json = survey.to_json()
+#json = st.to_json()
 #st.json(json)
